704d8e5cb9181e2902b0f1c1730516b4ba59479c
Marcos Figueiredo Salviano <marcos.salviano@cprm.gov.br>
2021-06-29 15:39:00 -0300

    Fill in the code for plot_ressuals.R

--- R/plot_residuals.R
+++ R/plot_residuals.R
@@ -1,7 +1,7 @@
 
 #' Make a plot of residuals vs fitted values
 #' 
-#' @param model an lm object.
+#' @param model an lm object. The function will extract residuals ans fitted values
 #' 
 #' @return a ggplot object
 #' 
@@ -26,18 +26,24 @@ plot_residuals <- function(model) {
   # ?fitted.values
   # ?residuals
   
+  fitted_values <- fitted.values(model)
+  resid <- residuals(model)
+  
   # Make a data frame with these two vectors using data.frame() 
   
-  residual_df <- data.frame()
+  residual_df <- data.frame(fitted_values,residuals = resid)
   
   # Now use the data frame with ggplot2
   
-  plot <- ggplot() + # Fill in the 'data' argument in this function
-    geom_point() + # Fill in the 'mapping' argument in this function
+  plot <- ggplot(data = residual_df) + # Fill in the 'data' argument in this function
+    geom_point(mapping = aes(x = fitted_values, y = residuals)) + # Fill in the 'mapping' argument in this function
     geom_hline(yintercept = 0) +
+    xlab("Fitted values") + ylab("Residual") + ggtitle ("Residual vs fitted values") +
     theme_bw()
   
+  plot
+  
   # We need to return our plot
-  return(0)
+  return(plot)
   
 }
